8cbba1019db529fa15e240e6458b6d055290fd43
tigerrex9 <33882494+tigerrex9@users.noreply.github.com>
2020-08-28 19:56:20 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "raylib.h"
+
+using namespace std;
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    int RectPos[4] = {screenWidth/2, screenHeight/2, screenWidth/4, screenHeight/4};
+
+    InitWindow(screenWidth, screenHeight, "My Game");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+            ClearBackground(BROWN);
+            DrawFPS(2,2);
+            DrawRectangle(RectPos[0],RectPos[1],RectPos[2],RectPos[3], BLUE);
+            DrawRectanglePro(Rectangle{200, 200, 100, 100}, Vector2{0,0}, 45.00f, GREEN);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
