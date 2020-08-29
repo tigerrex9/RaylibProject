a02308df8405adedc98f811efcc28b6c7fe92581
tigerrex9 <professorracecar@gmail.com>
2020-08-28 23:23:32 -0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,18 @@ using namespace std;
 int main() {
     const int screenWidth = 800;
     const int screenHeight = 450;
-    Rectangle rectangle = {200,200,100,100};
-    Vector2 recOrigin = {50,50};
-    float recRot = 45.0;
 
     InitWindow(screenWidth, screenHeight, "My Game");
-
     SetTargetFPS(60);
+
+    Texture2D Player = LoadTexture("../src/assets/Player.png");
 
     while (!WindowShouldClose())
     {
-        recRot += 0.5;
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawFPS(2,2);
-            DrawRectanglePro(rectangle, recOrigin, recRot, GREEN);
-
+            DrawTexture(Player, 100, 100, RAYWHITE);
         EndDrawing();
     }
 
